397ec7719b5e4594aa696c11681aefca9683c2fb
Mohamed Eldefrawy <Mohamed.Eldefrawy13026011@aiet.edu.eg>
2020-08-01 15:28:49 +0200

    Create unit test for HtmlFormatter

---
+++ TestNinja.NUnitTEsts/HtmlFormatterTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.NUnitTEsts
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        private HtmlFormatter _formatter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _formatter = new HtmlFormatter();
+        }
+        [Test]
+
+        [TestCase("HelloWorld!", "<strong>HelloWorld!</strong>")]
+        public void HtmlFormatter_WhenCalled_ShouldEncloseTheStringWithStrongElement(string input, string expected)
+        {
+            var result = _formatter.FormatAsBold(input);
+            Assert.That(result, Is.EqualTo(expected).IgnoreCase);
+        }
+    }
+}
